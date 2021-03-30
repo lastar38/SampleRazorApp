4e9d22eee7dc517dfb34d9685d2372a2ef5404a2
lastar38 <lastarmain@gmail.com>
2021-03-30 21:48:47 +0900

    3-23

--- SampleRazorApp/SampleRazorApp/Pages/Other.cshtml.cs
+++ SampleRazorApp/SampleRazorApp/Pages/Other.cshtml.cs
@@ -7,16 +7,41 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SampleRazorApp.Pages
 {
+    public enum Gender
+    {
+        male,
+        female
+    }
+
+    public enum Platform
+    {
+        Windows,
+        macOS,
+        Linux,
+        ChromeOs,
+        Android,
+        iOS
+    }
+
     public class OtherModel : PageModel
     {
         public string Message { get; set; }
+
+        public bool check { get; set; }
+
+        public Gender gender { get; set; }
+
+        public Platform pc { get; set; }
+
+        public Platform[] pc2 { get; set; }
         public void OnGet()
         {
+            Message = "check & select it!";
         }
 
-        public void OnPost()
+        public void OnPost(bool check, string gender, Platform pc, Platform[] pc2)
         {
-            Message = "you typed: " + Request.Form["msg"];
+            Message = "Result: " + check + "," + gender + "," + pc + ", " + pc2.Length;
         }
     }
 }
